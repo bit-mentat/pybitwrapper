87cbad3c2cd181c2a8ebf2b5666fe4e9dc5feb3b
bit-mentat <mentat4231@gmail.com>
2025-11-27 10:52:48 -0500

    Repl is woring, getting responses for single commands with no parameters but failing when passing passing one. Tried setting as string and passing as well

---
+++ main.py
@@ -0,0 +1,29 @@
+import pretty_json as pj
+import requests
+from decimal import Decimal
+import json
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+
+import vars   # ← this now works
+
+
+args = {"jsonrpc":"1.0","id":"pybitwrapper"}
+auth = (vars.user, vars.password)
+
+while True:
+    cmd = input("bitcoin-cli>")
+    test = cmd.split(" ")
+
+    args["method"] = test[0]
+    args["params"] = list(test[1])
+
+    
+
+    r = requests.post(vars.url, auth=auth, json=args, timeout=10)
+    
+    obj = json.loads(r.text, parse_float=Decimal)
+    print(pj.pretty_json(obj, indent=2))
+		
